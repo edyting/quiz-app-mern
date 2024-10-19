bc4bc2a3e53288cd2125535f1c2e0b46b6a3d25d
edyting <dankyiemmanuel0@gmail.com>
2024-10-19 10:23:14 +0000

    added the user services

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-
+const userRoutes = require("./routes/users")
 
 const app = express();
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 5000;
 let server;
+
+// middlewares
+app.use(cors());
+app.use(bodyparser.urlencoded({extended:true, limit:"20mb"}));
+app.use(bodyparser.json({limit:"20mb"}));
+
+app.use('/api/users/',userRoutes)
 
 // connecting to database
 mongoose.connect(process.env.DB_URI,{
@@ -19,6 +27,6 @@ mongoose.connect(process.env.DB_URI,{
 })
 
 
-server = app.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT} ...`)
 })
